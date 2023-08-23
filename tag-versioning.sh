cff348350d6a541baec168ea71846a692011aa0e
WengOnn-Deriv <weng.onn@deriv.com>
2023-08-23 16:42:28 +0800

    Clickup Integration - v1.11

--- tag-versioning.sh
+++ tag-versioning.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+set -e
+
 msg=$1
 
 if [[ ! -z ${msg} ]]
 then
     git add .
-    git commit -m "Clickup Integration - ${msg}"
+    git commit -m "Clickup Integration - ${msg}"  --allow-empty
     git tag -a -m "Clickup Integration - ${msg}" ${msg}
     git push origin master --follow-tags
 else
