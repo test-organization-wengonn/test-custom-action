94da119bf5d75dc2cc0424a74f7a8f7ba831cf46
WengOnn-Deriv <weng.onn@deriv.com>
2023-08-23 16:36:13 +0800

    Clickup Integration - v1.10

--- tag-versioning.sh
+++ tag-versioning.sh
@@ -7,7 +7,7 @@ then
     git add .
     git commit -m "Clickup Integration - ${msg}"
     git tag -a -m "Clickup Integration - ${msg}" ${msg}
-    git push --follow-tags
+    git push origin master --follow-tags
 else
     echo "Please pass in the tag version as a variable"
 fi 
