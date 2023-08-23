6f2e7a702f1f70bfc31f6bf055f7c9e5af7053e7
WengOnn-Deriv <weng.onn@deriv.com>
2023-08-23 12:33:06 +0800

    Clickup action

--- index.js
+++ index.js
@@ -2,14 +2,45 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
+    
+    const clickup_api_token = core.getInput('clickup_api_token', { required: true });
+    const pull_request_title = core.getInput('pull_request_title', { required: true });
+    const list_id = core.getInput('list_id', { required: true });
+    const team_id = core.getInput('team_id', { required: true });
+    
+    const query = new URLSearchParams({
+        custom_task_ids: 'true',
+        team_id: team_id
+    }).toString();
+
+    create_task(clickup_api_token, pull_request_title, list_id, query)
 } catch (error) {
-  core.setFailed(error.message);
+    core.setFailed(error.message);
 }
+
+
+async function create_task(clickup_api_token, pull_request_title, list_id, query){
+    const list_id = list_id;
+    const resp = await fetch(
+    `https://api.clickup.com/api/v2/list/${list_id}/task?${query}`,
+    {
+        method: 'POST',
+        headers: {
+        'Content-Type': 'application/json',
+        Authorization: clickup_api_token
+        },
+        body: JSON.stringify({
+        name: `Dependabot - ${pull_request_title}`,
+        description: 'New Task Description'
+        })
+    }
+    );
+    if (!resp.ok){
+        const message = `An error has occured: ${resp.status}`;
+        throw new Error(message);
+    }
+    
+    const data = await resp.json();
+    console.log(data);
+}
+
